a037792b55d90fd4ff96e5644e8e4cb1ce3824c0
Törehan Gören <torehangoren@gmail.com>
2021-11-25 22:09:30 +0100

    addItem

--- addItem.php
+++ addItem.php
@@ -1,4 +1,8 @@
 <?php
+$TEXT = $_POST['TEXT'];
+$COMPLETED = $_POST['COMPLETED'];
+$TOPIC_ID = $_POST['TOPIC_ID'];
+
 
 $servername = "10.35.232.44:3306";
 $username = "k169621_test";
@@ -12,7 +16,7 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
-$sql = "INSERT INTO todolist (text, completed, topic_id) VALUES ('Cay', '0', '1')";
+$sql = "INSERT INTO todolist (text, completed, topic_id) VALUES ('".$TEXT."', '".$COMPLETED."', '".$TOPIC_ID."')";
 
 if (mysqli_query($conn, $sql)) {
   echo "New record created successfully";
