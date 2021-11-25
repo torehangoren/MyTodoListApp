85cbb14f26f6622f22097b000068f5ff58dd7fb2
Törehan Gören <torehangoren@gmail.com>
2021-11-25 21:48:40 +0100

    arargaer

--- getItems.php
+++ getItems.php
@@ -12,9 +12,24 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
-$sql = "INSERT INTO todolist (TEXT, COMPLETED, TOPIC_ID)
-VALUES ('CHEESE', '0', '1')";
+$sql = "SELECT * FROM todolist";
+$result = mysqli_query($conn, $sql); 
+
+if(mysqli_num_rows($result) < 0)   // checking if there is any row in the resultset
+{
+	echo "There is no input in DB table";
+}
 
 
+ $res = array();
+while($row = mysqli_fetch_array($result)){
+ array_push($res, 
+ array('TEXT'=>$row[0],'COMPLETED'=>$row[1],'TOPIC_ID'=>$row[2]));
+ }
+
+
+ echo json_encode(array('result'=>$res));
+
+mysqli_free_result($result);
 mysqli_close($conn);
 ?>
